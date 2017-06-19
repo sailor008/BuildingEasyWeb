be029edab6fa0ab3fd9fa7a0467943c8f984fa79
zhenghongyi <zhenghongyi@zhenghongyideMacBook-Pro.local>
2017-06-19 15:01:26 +0800

    完善楼盘详情页逻辑

---
+++ BuildingEasyWeb/BuildingEasyWeb/楼盘/View/FormulaListCell.h
@@ -0,0 +1,17 @@
+//
+//  FormulaListCell.h
+//  BuildingEasyWeb
+//
+//  Created by zhenghongyi on 2017/6/19.
+//  Copyright © 2017年 zhenghongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BuildingDetailModel.h"
+
+@interface FormulaListCell : UITableViewCell
+
+@property (nonatomic, strong) FormulaModel* model;
+
+@end
