fa1961cc8867181093187ed85ab2a779826b0dc1
sailor008 <sailor_work@163.com>
2017-06-23 01:31:20 +0800

    提交icon图标，七牛云上传图片的简单接口

---
+++ BuildingEasyWeb/BuildingEasyWeb/Global/Model/QUYunManager.h
@@ -0,0 +1,24 @@
+//
+//  QUYunManager.h
+//  BuildingEasyWeb
+//
+//  Created by SeekMac on 2017/6/21.
+//  Copyright © 2017年 zhenghongyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef void(^UploadReqSuccess)(id reponseObj);
+typedef void(^UploadReqFailure)(NSError* error, NSString* reqId);
+
+
+@interface QUYunManager : NSObject
+
+
++(void)uploadData:(NSData*)data key:(NSString*) key token:(NSString*) token success:(UploadReqSuccess)successBlock failure:(UploadReqFailure)failureBlock;
+
++(void)uploadFileWithPath:(NSString*)filePath key:(NSString*) imgkey token:(NSString*) token success:(UploadReqSuccess)successBlock failure:(UploadReqFailure)failureBlock;
+
+
+@end
---
+++ BuildingEasyWeb/BuildingEasyWeb/Global/Model/QUYunManager.m
@@ -0,0 +1,82 @@
+//
+//  QUYunManager.m
+//  BuildingEasyWeb
+//
+//  Created by SeekMac on 2017/6/21.
+//  Copyright © 2017年 zhenghongyi. All rights reserved.
+//
+
+#import "QUYunManager.h"
+
+#import <QiniuSDK.h>
+
+@interface QUYunManager()
+
+@property (nonatomic, strong) QNUploadManager* qnUploadMgr;
+
+@end
+
+@implementation QUYunManager
+
+
++ (instancetype)shareQNYunManager
+{
+    static QUYunManager* manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[QUYunManager alloc] init];
+        
+        QNUploadManager *qnUploadMgr = [[QNUploadManager alloc]init];
+        manager.qnUploadMgr = qnUploadMgr;
+    });
+    return manager;
+}
+
++(void)uploadData:(NSData*)data key:(NSString*) imgkey token:(NSString*) token success:(UploadReqSuccess)successBlock failure:(UploadReqFailure)failureBlock
+{
+//    NSString *token = @"从服务端SDK获取";
+//    NSString *key = @"指定七牛服务上的文件名，或nil";
+//    NSString *filePath = @"要上传的文件路径";
+
+    [[QUYunManager shareQNYunManager].qnUploadMgr putData:data key:imgkey token:token complete:^(QNResponseInfo *info, NSString *key, NSDictionary *responseObj) {
+        if(info.ok)
+        {
+            NSLog(@"请求成功");
+        }
+        else{
+            NSLog(@"失败");
+            //如果失败，这里可以把info信息上报自己的服务器，便于后面分析上传错误原因
+        }
+        NSLog(@"info ===== %@", info);
+        NSLog(@"resp ===== %@", responseObj);
+    }
+    option:nil];
+}
+
++(void)uploadFileWithPath:(NSString*)filePath key:(NSString*) imgkey token:(NSString*) token success:(UploadReqSuccess)successBlock failure:(UploadReqFailure)failureBlock
+{
+    [[QUYunManager shareQNYunManager].qnUploadMgr putFile:filePath key:imgkey token:token complete:^(QNResponseInfo *info, NSString *key, NSDictionary *responseObj) {
+        NSLog(@"upload image result: info ===== %@", info);
+        NSLog(@"upload image result: responseData ===== %@", responseObj);
+        if(info.ok)
+        {
+            NSLog(@"请求成功");
+            successBlock(responseObj);
+        }
+        else{
+            NSLog(@"失败");
+            //如果失败，这里可以把info信息上报自己的服务器，便于后面分析上传错误原因
+            
+            failureBlock(info.error, info.reqId);
+        }
+
+    }
+    option:nil];
+}
+
+
+
+
+
+
+@end
