d431d53ba90d9b837444e227990dc2e368ee001b
zhenghongyi <zhenghongyi@zhenghongyideMacBook-Pro.local>
2017-07-08 09:39:49 +0800

    添加空白提示

---
+++ BuildingEasyWeb/BuildingEasyWeb/Global/View/EmptyTipView.h
@@ -0,0 +1,16 @@
+//
+//  EmptyTipView.h
+//  BuildingEasyWeb
+//
+//  Created by zhenghongyi on 2017/7/8.
+//  Copyright © 2017年 zhenghongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmptyTipView : UIView
+
++ (EmptyTipView *)GetEmptyTipView;
+@property (nonatomic, copy) NSString* tip;
+
+@end
---
+++ BuildingEasyWeb/BuildingEasyWeb/Global/View/EmptyTipView.m
@@ -0,0 +1,31 @@
+//
+//  EmptyTipView.m
+//  BuildingEasyWeb
+//
+//  Created by zhenghongyi on 2017/7/8.
+//  Copyright © 2017年 zhenghongyi. All rights reserved.
+//
+
+#import "EmptyTipView.h"
+
+@interface EmptyTipView ()
+
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@end
+
+@implementation EmptyTipView
+
++ (EmptyTipView *)GetEmptyTipView
+{
+    EmptyTipView* tipView = [[[NSBundle mainBundle] loadNibNamed:@"EmptyTipView" owner:nil options:nil] lastObject];
+    return tipView;
+}
+
+- (void)setTip:(NSString *)tip
+{
+    _tip = tip;
+    _tipLabel.text = tip;
+}
+
+@end
