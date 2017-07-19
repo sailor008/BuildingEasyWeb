3209a59709639992ebbe3b535065675d4837344f
zhenghongyi <zhenghongyi@zhenghongyideMacBook-Pro.local>
2017-07-19 22:19:09 +0800

    修复统计crash问题，修复图片控件和展示问题

--- BuildingEasyWeb/BuildingEasyWeb/我的/Controller/CustomerStatisticController.m
+++ BuildingEasyWeb/BuildingEasyWeb/我的/Controller/CustomerStatisticController.m
@@ -133,8 +133,8 @@
     _searchTxtField.text = @"";
     _nowStateModel = model;
     [self updateBtnNavTitle];
-    
-    [_baobeiInfoArr removeAllObjects];
+#warning 不要先清空数组内容再beginRefreshing，否则当tableview有被滑动过，这个操作顺序会触发到重用方法，带来crash
+//    [_baobeiInfoArr removeAllObjects];
     [_tableview.mj_header beginRefreshing];
 }
 
@@ -192,6 +192,10 @@
                                  };
     [NetworkManager postWithUrl:@"wx/getCustomerInfoByState" parameters:parameters success:^(id reponse) {
 //        NSLog(@">>>>>>>>>>>>>>>>>>>>>>>> %@", reponse);
+        if (_tableview.page == 1) {
+            [_baobeiInfoArr removeAllObjects];
+        }
+        
         NSArray* tmpArray = (NSArray *)reponse;
         NSInteger startIdx = (_tableview.page - 1) * pageSize;
         for (NSDictionary* dic in tmpArray) {
@@ -225,7 +229,7 @@
 {
 //    _isSearch = textField.text.length > 0;
 //    if (textField.text.length) {
-        [_baobeiInfoArr removeAllObjects];
+//        [_baobeiInfoArr removeAllObjects];
         [_tableview.mj_header beginRefreshing];
 //    }
 }
@@ -234,7 +238,7 @@
     //收起键盘
     [_searchTxtField resignFirstResponder];
     //刷新列表
-    [_baobeiInfoArr removeAllObjects];
+//    [_baobeiInfoArr removeAllObjects];
     [_tableview.mj_header beginRefreshing];
     return YES;
 }
